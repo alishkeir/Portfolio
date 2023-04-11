6f7cccf6ed264d3665768b90a68546ea6489f840
alishkeir <ali.shkeir@itxi.net>
2023-04-11 14:58:33 +0300

    UI Improvements and Added new form

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,67 +142,6 @@
         return false;
     });
 
-    // jQuery counterUp
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 1000,
-    });
-
-    // Skills section
-    $('.skills-content').waypoint(
-        function () {
-            $('.progress .progress-bar').each(function () {
-                $(this).css('width', $(this).attr('aria-valuenow') + '%');
-            });
-        },
-        {
-            offset: '80%',
-        }
-    );
-
-    // Porfolio isotope and filter
-    $(window).on('load', function () {
-        var portfolioIsotope = $('.portfolio-container').isotope({
-            itemSelector: '.portfolio-item',
-            layoutMode: 'fitRows',
-        });
-
-        $('#portfolio-flters li').on('click', function () {
-            $('#portfolio-flters li').removeClass('filter-active');
-            $(this).addClass('filter-active');
-
-            portfolioIsotope.isotope({
-                filter: $(this).data('filter'),
-            });
-            aos_init();
-        });
-
-        // Initiate venobox (lightbox feature used in portofilo)
-        $(document).ready(function () {
-            $('.venobox').venobox();
-        });
-    });
-
-    // Testimonials carousel (uses the Owl Carousel library)
-
-    $('.testimonials-carousel').owlCarousel({
-        autoplay: 30000,
-        dots: true,
-        loop: true,
-        items: 3,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [992, 2],
-        itemsTablet: [767, 1],
-    });
-
-    // Portfolio details carousel
-    $('.portfolio-details-carousel').owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        items: 1,
-    });
-
     // Init AOS
     function aos_init() {
         AOS.init({
@@ -214,12 +153,4 @@
     $(window).on('load', function () {
         aos_init();
     });
-
-    // function toggleTheme() {
-    //     $('#body').toggleClass('light dark');
-    // }
 })(jQuery);
-
-function toggleTheme() {
-    $('body').toggleClass('light dark');
-}
